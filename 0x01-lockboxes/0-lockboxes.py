3ca1788db4a1375fdbebbea034f7e49cb517fed4
miheret-eng <mihherettad2007@gmail.com>
2022-07-22 00:30:42 -0700

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,31 @@
+#!/usr/bin/python3
+"""LockBoxes - ALX"""
+
+
 def canUnlockAll(boxes):
-    unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
+    """determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    """
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < len(boxes):
+        old = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < len(boxes) and key not in opened_boxes:
+                i = key
+                break
+        if old != i:
             continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+        else:
+            break
+
+    for i in range(len(boxes)):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
